61b06b6c8905e997b187b59d47e9912921e3fe41
GINA <gina.xiee@gmail.com>
2015-04-05 22:38:57 -0400

    Voting Works!

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 		<!-- 3 Project Previews-->
 		<center>
-		<h1 id="topic_heading"> Top Six Projects in <font color=#00FFCC>Fund</font><font color=#0066FF>Book</font> </h1>
+		<h1 id="topic_heading"> Top Three Projects in <font color=#00FFCC>Fund</font><font color=#0066FF>Book</font> </h1>
 		
 		<hr color="#6699FF" size="6"> 
 		<div class="row">
@@ -10,18 +10,18 @@
 			$dsn = "mysql:host=localhost;dbname=fundbook";
 			$db = new PDO($dsn, "root", "fundbook");
 
-			$projects=$db->query("SELECT * FROM projects LIMIT 6");
+			$projects=$db->query("SELECT * FROM projects LIMIT 3");
 			if ($projects->rowCount() > 0) {
 				foreach ($projects as $project) {
 					echo  '<div class="col-sm-6 col-md-4">
 						    <div class="thumbnail">
-						      <img src= ' . $project["picture"] . ' alt="...">
+						      <img src= ' . $project["picture"] . ' alt="..." width="450" height="300">
 						      <div class="caption">
 						        <h3>'. $project["name"] .'</h3>
 						        <p>' . $project["description"] . '</p>
 						        <p><a href="example.php?pid='. $project["pid"] . 
 						        			'" class="btn btn-primary" role="button">View Page</a> 
-						        	<a href="#" class="btn btn-default" role="button">Add to Interests</a>
+						        	<a href="" class="btn btn-default" role="button" val="like" pid="'. $project["pid"] . '">Add To Interests</a>
 						        </p>
 						      </div>
 						    </div>
